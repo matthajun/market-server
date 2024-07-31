c288a5568c49c6ce857ba0de1e4c1bec27d1d2e3
Ralph Park <137850902+Ralph-DoubleMe-Me@users.noreply.github.com>
2024-07-31 21:13:24 +0900

    refactor: grpc

--- src/configs/grpc.config.ts
+++ src/configs/grpc.config.ts
@@ -3,40 +3,6 @@ import { join } from 'path';
 import { ClientOptions, Transport } from '@nestjs/microservices';
 
 /**
- * media-file-upload 서버에 상품(기획) 행사 이미지 업로드 요청
- * @description Get gRPC client options
- *   package: grpc 통신할 때 사용할 proto 파일의 package 이름
- *   url: gRPC 서버의 주소 (웹서버와 다른 포트로 열어야 함)
- *   keepaliveTimeoutMs: gRPC 서버의 keepalive timeout = 서버 타임아웃 시간과 동일하게 설정
- *   protoPath: proto 파일의 경로
- *   keepCase: proto 파일의 camelCase를 유지할지 여부
- * @returns gRPC ClientOptions
- *
- */
-export function getGrpcPlanningEventImageUploadClientOptions(): ClientOptions {
-  const MEDIA_FILE_UPLOAD_SERVER_GRPC_ENDPOINT =
-    process.env.MEDIA_FILE_UPLOAD_SERVER_GRPC_ENDPOINT;
-
-  if (!MEDIA_FILE_UPLOAD_SERVER_GRPC_ENDPOINT) {
-    throw new Error('MEDIA_FILE_UPLOAD_SERVER_GRPC_ENDPOINT env required');
-  }
-
-  return {
-    transport: Transport.GRPC,
-    options: {
-      package: 'planning_event_image_upload_proto',
-      url: MEDIA_FILE_UPLOAD_SERVER_GRPC_ENDPOINT,
-      protoPath: join(
-        __dirname,
-        '../util/grpc/proto/planning_event_image_upload.proto',
-      ),
-      loader: { keepCase: true },
-      keepalive: { keepaliveTimeoutMs: 61000 },
-    },
-  };
-}
-
-/**
  * admin-server에 토큰을 검증하기 위한 gRPC 옵션
  * @description Get gRPC client options
  *   package: grpc 통신할 때 사용할 proto 파일의 package 이름
