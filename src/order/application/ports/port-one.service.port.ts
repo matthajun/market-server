8f98ef7df93d37e34527c4b0d3679e8cad629309
Ralph Park <137850902+Ralph-DoubleMe-Me@users.noreply.github.com>
2024-08-04 22:55:18 +0900

    refactor: delete not used port

--- src/order/application/ports/port-one.service.port.ts
+++
@@ -1,7 +0,0 @@
-import { Order } from '@src/order/domain/order';
-import { OrderHistoryReadModel } from '@src/order/domain/read-models/order-history.read-model';
-
-export abstract class PortOneServicePort {
-  abstract prepare(order: Order): Promise<OrderHistoryReadModel>;
-  abstract validateOrder(transactionId: string): Promise<boolean>;
-}
